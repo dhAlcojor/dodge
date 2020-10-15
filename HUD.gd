2d3eee9453946289dd1afde546c3d4f1cd1bca2b
dhalcojor <danheralc@gmail.com>
2020-10-16 00:00:43 +0200

    First version of the game finished

---
+++ HUD.gd
@@ -0,0 +1,41 @@
+extends CanvasLayer
+
+signal start_game
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+  pass # Replace with function body.
+  
+  
+func _on_MessageTimer_timeout() -> void:
+  $Message.hide()
+
+
+func _on_StartButton_pressed() -> void:
+  $StartButton.hide()
+  emit_signal('start_game')
+
+
+func show_message(text):
+  $Message.text = text
+  $Message.show()
+  $MessageTimer.start()
+
+
+func show_game_over():
+  show_message('Game Over')
+  
+  # Wait until the MessageTimer has counted down
+  yield($MessageTimer, 'timeout')
+  
+  $Message.text = 'Dodge the Creeps!'
+  $Message.show()
+  
+  # Make a one-shot timer and wait for it to finish
+  yield(get_tree().create_timer(1), 'timeout')
+  $StartButton.show()
+
+
+func update_score(score):
+  $ScoreLabel.text = str(score)
